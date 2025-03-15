f5263d41439c8c04bcf223e757c07d2f1ce29de6
yanmakhnis <makhnis@gmail.com>
2025-03-15 13:25:28 +0300

    added hw6, hw7, cw8

--- homework/homeTask06/script.js
+++ homework/homeTask06/script.js
@@ -1,16 +1,33 @@
-const elements = document.querySelectorAll('[class^="box"]');
+// const elements = document.querySelectorAll('[class^="box"]');
 
-elements.forEach((element) => {
-    element.addEventListener('click', handleClick)
-});
+// elements.forEach((element) => {
+//     element.addEventListener('click', handleClick)
+// });
+
+// function handleClick(event) {
+//     const clickX = event.clientX;
+//     const clickY = event.clientY;
+//     const elementTitleData = event.currentTarget.getAttribute('data-title');
+//     const elementDimension = elementTitleData.split(' ')[1]
+
+//     console.log(`Координаты клика ${clickX},${clickY}`)
+//     console.log(`Размер прямоугольника - ${elementDimension}`)
+//     console.log('Заголовок прямоугольника:', elementTitleData);
+// }
+
+const element = document.querySelector('.wrapper');
+element.addEventListener('click', handleClick)
 
 function handleClick(event) {
     const clickX = event.clientX;
     const clickY = event.clientY;
-    const elementTitleData = event.currentTarget.getAttribute('data-title');
-    const elementDimension = elementTitleData.split(' ')[1]
-    
-    console.log(`Координаты клика ${clickX},${clickY}`)
-    console.log(`Размер прямоугольника - ${elementDimension}`)
-    console.log('Заголовок прямоугольника:', elementTitleData);
+    const clickedElement = document.elementFromPoint(clickX, clickY);
+   
+    if(clickedElement.className.includes('box')){
+        const elementTitleData = clickedElement.getAttribute('data-title');
+        const elementDimension = elementTitleData.split(' ')[1];
+        console.log(`Координаты клика ${clickX},${clickY}`);
+        console.log(`Размер прямоугольника - ${elementDimension}`);
+        console.log('Заголовок прямоугольника:', elementTitleData);
+    }  
 }
---
+++ homework/homeTask07/script.js
@@ -0,0 +1,32 @@
+const userStateWrapper = document.querySelector('#user-state');
+
+document.addEventListener("DOMContentLoaded", onPageLoad);
+
+function onPageLoad(event) {
+    let sessionId = getCookie('sessionId');
+    if(sessionId){
+         userStateWrapper.textContent = "Авторизированный пользователь"
+    }
+    else {
+        if(confirm("Хотите авторизоваться?")){
+            const now = new Date();
+            const sessionId = `${now.getFullYear()}${now.getMonth()}${now.getDate()}${now.getHours()}${now.getMinutes()}`;
+            document.cookie = `sessionId=${sessionId}; max-age=60`;
+            location.reload();
+        }
+    } 
+}
+
+function getCookie(name) {
+    const cookieString = document.cookie;
+    console.log(cookieString)
+    const cookies = cookieString.split('; ');
+
+    for (let cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split('=');
+        if (cookieName === name) {
+            return cookieValue; 
+        }
+    }
+    return null; 
+}
